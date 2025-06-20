c75a263e1f864a1b879b078f1c03a665c5c11e14
moonwhaler <me@nordinary.com>
2025-06-20 13:07:30 +0200

    Renamed the node file.

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from .proportional_dimension_node import NODE_CLASS_MAPPINGS as pd_class_mapping
 from .string_replace_nodes import NODE_CLASS_MAPPINGS as sr_class_mappings, NODE_DISPLAY_NAME_MAPPINGS as sr_display_mappings
 from .vace_looper_fade_mask_node import NODE_CLASS_MAPPINGS as vls_class_mappings, NODE_DISPLAY_NAME_MAPPINGS as vls_display_mappings
 from .dynamic_string_concat_node import NODE_CLASS_MAPPINGS as dsc_class_mappings, NODE_DISPLAY_NAME_MAPPINGS as dsc_display_mappings
-from .dynamic_lora_stack import NODE_CLASS_MAPPINGS as dls_class_mappings, NODE_DISPLAY_NAME_MAPPINGS as dls_display_mappings
+from .dynamic_lora_stack_node import NODE_CLASS_MAPPINGS as dls_class_mappings, NODE_DISPLAY_NAME_MAPPINGS as dls_display_mappings
 
 # Combine the mappings
 NODE_CLASS_MAPPINGS = {
